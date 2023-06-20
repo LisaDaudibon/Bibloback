d867d86d0371cfacbd04d11ed20bb3bcf1eeec92
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-20 16:24:33 +0200

    LD modifiying belongs_to database

--- db/migrate/20230620102839_create_reading_lists.rb
+++ db/migrate/20230620102839_create_reading_lists.rb
@@ -1,7 +1,6 @@
 class CreateReadingLists < ActiveRecord::Migration[7.0]
   def change
     create_table :reading_lists do |t|
-      t.boolean :read
       t.belongs_to :user, index: true
 
       t.timestamps
