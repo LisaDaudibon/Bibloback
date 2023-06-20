fd042b2eebbaddeb892361e1f3a816d86dee7eee
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-20 20:28:07 +0200

    register user with pseudo through json request okay on insomnia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :configure_devise_parameters, if: :devise_controller?
 
   def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:pseudo, :email, :password, :password_confirmation, :current_password)}
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:pseudo, :email, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:pseudo, :email, :password, :password_confirmation, :current_password)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:pseudo, :email, :password, :password_confirmation, :current_password)
+    end
   end
+  
 end
