b0bcede048f55e4763b3be567108c9006191be5a
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-21 21:55:32 +0200

    LD test model and factory reading goal

--- spec/factories/reading_goals.rb
+++ spec/factories/reading_goals.rb
@@ -1,12 +1,10 @@
-require 'date'
-
 FactoryBot.define do
   factory :reading_goal do
     pages { 223 }
     complete { true }
     rating { 9 }
-    start_date { 01/02/2022 }
-    end_date { 11/06/2022 }
+    start_date { DateTime.now }
+    end_date { Date.new(2023,7,3) }
     association :user
   end
 end
