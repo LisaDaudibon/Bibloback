0709299f55b5e9d75954e368716078f0fb54c46e
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-21 12:49:55 +0200

    mise en place de la creation user dans le test avec factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = FactoryBot.create(:user)
+  # inclus les param de factory
+  before(:all) do
+    FactoryBot.reload
   end
 
-  describe "email" do
-    it { expect(@user).to validate_presence_of(:email) }
+  #  FactoryBot cree l'objet user
+  let(:user) { FactoryBot.build(:user) }
+
+  # ...
+  describe 'factory user' do
+    it 'creates a valid user' do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+  
+    it 'creates a persisted user' do
+      user = FactoryBot.create(:user)
+      expect(user.persisted?).to be true
+    end
+  end
+
+  describe 'validations' do
+
+   
   end
 
 end
