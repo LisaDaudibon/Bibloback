29af423b040e4737a5afce12ae76744af90ba0fd
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-21 12:54:46 +0200

    password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,27 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
 
+    it 'validates email format' do
+      user.email = 'invalid_email'
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include('is invalid')
+    end
+    
+    it 'validates presence of password' do
+      user.password = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it 'validates password confirmation matches the password' do
+      user.password = 'password'
+      user.password_confirmation = 'different_password'
+      expect(user).not_to be_valid
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
    
+  
   end
 
 end
