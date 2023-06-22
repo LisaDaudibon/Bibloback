29493e19588ce7ae4a097af014147b60a95d87cd
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-22 11:19:59 +0200

    LD root

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       }
     get '/member-data', to: 'members#show'
 
+  root "books#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
