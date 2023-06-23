95c5a485e4b4914a18b174ed001fe30f082652ad
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-23 10:29:17 +0200

    LD expiration time

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,6 +18,7 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
 		jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.expiration_time = 120.minutes.to_i
 	end
 
   # ==> Controller configuration
