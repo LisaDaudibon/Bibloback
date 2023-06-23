724e13018f5c7c7c9f13d0730dec82c474782919
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-23 17:16:45 +0200

    LD adding false to user admin

--- db/migrate/20230620165218_add_pseudo_to_users.rb
+++ db/migrate/20230620165218_add_pseudo_to_users.rb
@@ -1,6 +1,6 @@
 class AddPseudoToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :pseudo, :string
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
