721997759428ff0c66bb557680847cdf8af4aeed
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-28 11:50:20 +0200

    creation reading_items controller

---
+++ app/controllers/reading_items_controller.rb
@@ -0,0 +1,51 @@
+class ReadingItemsController < ApplicationController
+  before_action :set_reading_item, only: %i[ show update destroy ]
+  
+  # GET /reading_items
+  def index
+    
+    @reading_items = ReadingItem.all
+    render json: @reading_items
+  end
+
+  # GET /reading_items/1
+  def show
+    render json: @reading_item
+  end
+
+  # POST /reading_items
+  def create
+    @reading_item = ReadingItem.new(reading_item_params)
+
+    if @reading_item.save
+      render json: @reading_item, status: :created, location: @reading_item
+    else
+      render json: @reading_item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /reading_items/1
+  def update
+    if @reading_item.update(reading_item_params)
+      render json: @reading_item
+    else
+      render json: @reading_item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /reading_items/1
+  def destroy
+    @reading_item.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_reading_item
+      @reading_item = ReadingGoal.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def reading_item_params
+      params.require(:reading_item).permit(:pages, :complete, :rating, :start_date, :end_date)
+    end
+end
