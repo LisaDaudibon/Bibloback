03b572be63066fb2411690b844873232c6386dbd
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-28 15:10:03 +0200

    validate only if precense true

--- app/models/reading_goal.rb
+++ app/models/reading_goal.rb
@@ -3,8 +3,22 @@ class ReadingGoal < ApplicationRecord
   has_many :reading_items, dependent: :destroy
   has_many :books, through: :reading_items
 
-  validates :pages, numericality: { only_integer: true }
-  validates :rating, numericality: { only_integer: true }
-  validates :complete, inclusion: [true, false]
-  validates :start_date, presence: true
+  validates :pages, numericality: { only_integer: true }, if: :pages_present?
+  validates :rating, numericality: { only_integer: true }, if: :rating_present?
+  validates :start_date, presence: true, if: :start_date_present?
+
+  private
+
+  def pages_present?
+    pages.present?
+  end
+
+  def rating_present?
+    rating.present?
+  end
+
+  def start_date_present?
+    start_date.present?
+  end
+
 end
